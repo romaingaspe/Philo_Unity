06da9107b602b086ff9646f2da792c6518d80fa5
martinatVincent <offres@vincentmartinat.com>
2016-02-16 14:32:08 +0100

    footer et layout
    creation footer
    layout d'origine retrait footer.

---
+++ app/templates/footer.php
@@ -0,0 +1,12 @@
+<div class="container section">
+		<section>
+			<?= $this->section('main_content') ?>
+		</section>
+		<footer>
+		</footer>
+	</div>
+	<script type="text/javascript" src="<?= $this->assetUrl('js/jquery-2.2.0.js') ?>"></script>
+	<script type="text/javascript" src="<?= $this->assetUrl('js/materialize.js') ?>"></script>
+
+</body>
+</html>
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -21,15 +21,4 @@
 			</div>
 		</nav>
 	</header>
-	<div class="container section">
-		<section>
-			<?= $this->section('main_content') ?>
-		</section>
-		<footer>
-		</footer>
-	</div>
-	<script type="text/javascript" src="<?= $this->assetUrl('js/jquery-2.2.0.js') ?>"></script>
-	<script type="text/javascript" src="<?= $this->assetUrl('js/materialize.js') ?>"></script>
-
-</body>
-</html>
+	
