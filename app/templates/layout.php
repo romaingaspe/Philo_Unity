e27b288e22d2fddcc2692a6c4afc3b4a135520f8
GetHobbit <masgeof@gmail.com>
2016-02-16 12:00:02 +0100

    Nav + inté LOGO

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -5,13 +5,16 @@
 	<title><?= $this->e($title) ?></title>
 
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
 <body>
 	<header>
 		<nav>
 			<div class="container">
+				<a href="/Philo_Unity/public">
+					<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
+				</a>
 				<ul class="right">
-					<li><a href="/Philo_Unity/public">Home</a></li>
 					<li><a href="metier">Métiers</a></li>
 					<li><a href="profil">Profils</a></li>
 					<li><a href="login">Login</a></li>
