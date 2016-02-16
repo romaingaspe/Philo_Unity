04663589f294d1c8ea9b0e60e7946cec86741ba9
martinatVincent <offres@vincentmartinat.com>
2016-02-16 11:58:09 +0100

    changer les routes

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,5 @@
 <?php
-
+	
 	$w_routes = array(
-		['GET', '/', 'Front#index', 'index'],
-		['GET', '/profil', 'Front#profil', 'profil'],
-		['GET', '/metier', 'Front#metier', 'metier'],
-		['GET|POSt', '/login', 'Back#login', 'login'],
+		['GET', '/', 'Default#home', 'home'],
 	);
