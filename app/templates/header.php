eccc5b7669136d8213bea7803c96edfca6e4990e
FatOuch <designdif@live.fr>
2016-02-16 14:34:09 +0100

    header/layout
    - création du header
    - retrait du header du layout d'origine

---
+++ app/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $this->e($title) ?></title>
+
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+</head>
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="UTF-8">
-	<title><?= $this->e($title) ?></title>
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
-	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
-</head>
 <body>
 	<header>
 		<nav>
