5376efe16928876ea79e0b9753d5f26c94d32cd5
GetHobbit <masgeof@gmail.com>
2016-02-16 17:01:44 +0100

    Suppr content

--- app/templates/front/contactAdmin.php
+++ app/templates/front/contactAdmin.php
@@ -1,5 +1,9 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>contact_admin</h1>
+
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('script') ?>
+
+<?php $this->stop('script') ?>
