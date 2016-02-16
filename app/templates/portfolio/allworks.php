6bd9eef721ee21c8a22e05715716cc22aabacb38
FatOuch <designdif@live.fr>
2016-02-16 20:08:27 +0100

    Construction pages
    + pages : allworks / profilsAlls / galleryProjects
    + imports : 2 photos
    + modif : page css

--- app/templates/portfolio/allworks.php
+++ app/templates/portfolio/allworks.php
@@ -1,5 +1,72 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
+	
 <?php $this->start('main_content') ?>
-<h1>all_works</h1>
+	<div id="main_photo" class="center-align">
+		<h5>Propulsé par:</h5>
+		<img class="bigphoto" src="/Philo_Unity/public/assets/img/philo.jpg" alt="">
+	</div>
+	<h1 class="center-align">Nos métiers</h1>
+	<section id="allworks" class="row">
+	<!-- les articles ne doivent être cliquables que si il y a du contenu généré(voir avec js) -->
+		<article class="col l4">
+			<div class="photo-work">
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt=""><!-- couture.jpg -->
+			</div>
+			<div class="text-works">
+				<h6>Couture</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum consectetur eveniet odio aliquam dolores repudiandae, nobis consequuntur inventore incidunt illo rerum, iusto, dolor voluptatum temporibus soluta magni, facilis quibusdam ab?</p>
+			</div>
+		</article>
+
+		<article class="col l4">
+			<div>
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt="">
+			</div>	
+			<div class="text-works">
+				<h6>Marqueterie</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum consectetur eveniet odio aliquam dolores repudiandae, nobis consequuntur inventore incidunt illo rerum, iusto, dolor voluptatum temporibus soluta magni, facilis quibusdam ab?</p>
+			</div>
+		</article>
+		<article class="col l4">
+			<div>
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt="">
+			</div>	
+			<div class="text-works">
+				<h6>Sculpture</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae voluptatem amet, doloribus, facere dolorum numquam temporibus praesentium esse facilis officiis ipsum nesciunt, ad distinctio sequi autem magni tenetur ex. Laboriosam.</p>
+			</div>
+		</article>
+		<article class="col l4">
+			<div>
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt="">
+			</div>	
+			<div class="text-works">
+				<h6>Peinture</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum libero mollitia iure ipsa! Sint, eveniet, unde doloremque commodi omnis deleniti quibusdam nulla reiciendis illo velit magni fugiat atque incidunt provident.</p>
+			</div>
+		</article>
+		<article class="col l4">
+			<div>
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt="">
+			</div>	
+			<div class="text-works">
+				<h6>Menuiserie</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, vel obcaecati, deleniti corporis minus inventore facere. Architecto voluptatibus nisi facilis. Fugiat, consequuntur, sint. Alias impedit sequi quod, nulla eveniet consequatur.</p>
+			</div>
+		</article>
+		<article class="col l4">
+			<div>
+				<img src="/Philo_Unity/public/assets/img/empty_works.png" alt="">
+			</div>	
+			<div class="text-works">
+				<h6>Arts-Plastiques</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur minus harum, laboriosam commodi ipsum nemo accusamus perferendis consectetur aliquid laudantium? Veritatis in dicta fuga ex inventore quas minima laboriosam sit!</p>
+			</div>
+		</article>
+	</section>
+	<section id="pagination" class="row">
+		<span>1</span>
+	</section>
+
 <?php $this->stop('main_content') ?>
--- app/templates/portfolio/galleryProjects.php
+++ app/templates/portfolio/galleryProjects.php
@@ -1,5 +1,58 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>gallery_projects</h1>
+	<span><a href="">portefolio></a><a href="">gallerie des projets></a></span>
+	<section id="presentation">
+		<div class="row">
+			<div class="col l6">
+				<img src="" alt="">
+			</div>
+			<div class="col l6">
+				<h4>Paul-Emile DOUDAIN</h4>
+				<h6>Marquetteur</h6>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus vero dolor quidem possimus dicta consequatur aliquam, soluta minima voluptatibus officia maiores ipsam veritatis dolore dolorum eum, eligendi modi a commodi?</p>
+			</div>
+		</div>
+	</section>
+	<section id="gallery">
+		<div class="row">
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+
+		<article class="col l4">
+			<h7></h7>
+			<div><img src="" alt=""></div>
+			<p></p>
+		</article>
+			
+		</div>
+	</section>
+
 <?php $this->stop('main_content') ?>
