1c33213dfa04a43f5e008451212751d2e2e2224a
FatOuch <designdif@live.fr>
2016-02-16 15:24:42 +0100

    retrait/ajout nav
    Retrait de la balise header(partie nav) de tous les layouts (layout
    d'origine compris ) et ajout dans le require header.php

--- app/templates/header.php
+++ app/templates/header.php
@@ -6,3 +6,18 @@
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
+<body>
+	<header>
+		<nav>
+			<div class="container">
+				<a href="/Philo_Unity/public">
+					<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
+				</a>
+				<ul class="right">
+					<li><a href="/Philo_Unity/public/metiers">Métiers</a></li>
+					<li><a href="/Philo_Unity/public/profils">Profils</a></li>
+					<li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li>
+				</ul>
+			</div>
+		</nav>
+	</header>
--- app/templates/layout_updates_pages.php
+++ app/templates/layout_updates_pages.php
@@ -1,19 +1,5 @@
 
 <?php require_once'header.php' ?>
-<body>
-	<header>
-		<nav>
-			<div class="container">
-				<a href="/Philo_Unity/public">
-					<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
-				</a>
-				<ul class="right">
-					<li><a href="/Philo_Unity/public/metiers">Métiers</a></li>
-					<li><a href="/Philo_Unity/public/profils">Profils</a></li>
-					<li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
+
 
 <?php require_once'footer.php' ?>
