61dadc7d5e45c0b9e5a65b66ef2cedf4658a5e17
defelite <lanfranchi.guillaume@yahoo.com>
2016-02-16 17:05:40 +0100

    contact

--- app/templates/profil/contact.php
+++ app/templates/profil/contact.php
@@ -2,4 +2,29 @@
 
 <?php $this->start('main_content') ?>
 <h1>contact</h1>
+
+<div class="row">
+    <form method="POST" class="col l6">
+        <div class="input-field">
+            <label for="nom">Nom</label>
+            <input type="text" name="nom" placeholder="Votre nom">
+        </div>
+        <div class="input-field">
+            <label for="objet">Objet</label>
+            <input type="text" name="objet" placeholder="Votre Objet">
+        </div>        
+        <div class="input-field">
+            <label for="email">E-mail</label>
+            <input type="text" name="email" placeholder="Votre email">
+        </div>
+        <div class="input-field">
+            <label for="email">Message</label>
+            <textarea name="message" class="materialize-textarea" placeholder="Votre message"></textarea>
+        </div>
+        <div class="input-field">
+            <input type="submit" name="but" value="Envoyer" class="waves-effect waves-light btn">
+        </div>
+    </form>
+</div>
+
 <?php $this->stop('main_content') ?>
