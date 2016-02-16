d9b5ba25f1f96ba3442128ca8bcbd60948a6b96b
FatOuch <designdif@live.fr>
2016-02-16 15:00:53 +0100

    rien de spé : retrait d'un retour à la ligne

--- app/templates/header.php
+++ app/templates/header.php
@@ -3,7 +3,6 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
 </head>
