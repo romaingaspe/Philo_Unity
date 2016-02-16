1fba4aa2e26cbc2742bdd1909a6f0ee2dd531e0b
GetHobbit <masgeof@gmail.com>
2016-02-16 16:13:30 +0100

    decoupage layout - header et footer dans partial/

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -1,16 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $this->e($title) ?></title>
+
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+</head>
 <body>
-	<header>
-		<nav>
-			<div class="container">
-				<a href="/Philo_Unity/public">
-					<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
-				</a>
-				<ul class="right">
-					<li><a href="/Philo_Unity/public/metiers">Métiers</a></li>
-					<li><a href="/Philo_Unity/public/profils">Profils</a></li>
-					<li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
-	
+	<?php $this->insert('partials/header') ?>
+
+	<div class="container section">
+		<section>
+			<?= $this->section('main_content') ?>
+		</section>
+	</div>
+
+	<?php $this->insert('partials/footer') ?>
+
+	<script type="text/javascript" src="<?= $this->assetUrl('js/jquery-2.2.0.js') ?>"></script>
+	<script type="text/javascript" src="<?= $this->assetUrl('js/materialize.js') ?>"></script>
+
+	<?= $this->section('scripts') ?>
+</body>
+</html>
---
+++ app/templates/partials/header.php
@@ -0,0 +1,14 @@
+<header>
+	<nav>
+		<div class="container">
+			<a href="/Philo_Unity/public">
+				<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
+			</a>
+			<ul class="right">
+				<li><a href="/Philo_Unity/public/metiers">Métiers</a></li>
+				<li><a href="/Philo_Unity/public/profils">Profils</a></li>
+				<li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
