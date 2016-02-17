5c29f4f7770f10b89e7840fc0f7ed87eddd37803
defelite <lanfranchi.guillaume@yahoo.com>
2016-02-17 11:32:29 +0100

    contact
    contact à améliorer par captcha

--- app/templates/admin/deconnectTotale.php
+++ app/templates/admin/deconnectTotale.php
@@ -1,5 +1,6 @@
-<?php $this->layout('layout', ['title' => 'Login']) ?>
+<?php $this->layout('layout2', ['title' => 'Login']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>deconnect_totale</h1>
+<h1>Vous êtes déconnecté(e)</h1>
+<a href="/">Retour à l'accueil</a>
 <?php $this->stop('main_content') ?>
