26300a57caf8ef516e601babb9506d74cbbdd7d6
GetHobbit <masgeof@gmail.com>
2016-02-17 11:56:25 +0100

    Modification Index

--- app/templates/front/index.php
+++ app/templates/front/index.php
@@ -2,17 +2,7 @@
 
 <?php $this->start('main_content') ?>
 
-<style type="text/css">
-	.image_index img {
-		width: 100%;
-		height: 400px;
-}
-</style>
-
-		<div class="image_index">	
-			<img src="http://static.panoramio.com/photos/original/12271445.jpg">
-		</div>
-
-		<h5>INDEX</h5>
-
+<div class="responsive-img">
+	<img src="<?= $this->assetUrl('img/philo.jpg') ?>">
+</div>
 <?php $this->stop('main_content') ?>
