ef4515651571e4a63b348ea504ffed61488e016f
GetHobbit <masgeof@gmail.com>
2016-02-17 19:42:32 +0100

    Before sur le footer pour centrer verticalement le carousel

--- app/templates/front/index.php
+++ app/templates/front/index.php
@@ -8,11 +8,33 @@
 		<img class="col l6 offset-l3"src="<?= $this->assetUrl('img/LogoPhilomathique.png') ?>" alt="" />
 	</div>
 </div>
-<div class="carousel">
-	 <a class="carousel-item" href="#one!"><img src="http://lorempixel.com/600/600/nature/1"></a>
-	 <a class="carousel-item" href="#two!"><img src="http://lorempixel.com/600/600/nature/2"></a>
-	 <a class="carousel-item" href="#three!"><img src="http://lorempixel.com/600/600/nature/3"></a>
-	 <a class="carousel-item" href="#four!"><img src="http://lorempixel.com/600/600/nature/4"></a>
-	 <a class="carousel-item" href="#five!"><img src="http://lorempixel.com/600/600/nature/5"></a>
- </div>
+<div id="carous" class="">
+	<div class="carousel">
+		<a class="carousel-item" href="#one!">
+			<h2 class="center">Machin truc</h2>
+			<img src="http://lorempixel.com/600/600/people/1">
+			<p class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		</a>
+		<a class="carousel-item" href="#two!">
+			<h2 class="center">Machin truc</h2>
+			<img src="http://lorempixel.com/600/600/people/2">
+			<p class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		</a>
+		<a class="carousel-item" href="#three!">
+			<h2 class="center">Machin truc</h2>
+			<img src="http://lorempixel.com/600/600/people/3">
+			<p class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		</a>
+		<a class="carousel-item" href="#four!">
+			<h2 class="center">Machin truc</h2>
+			<img src="http://lorempixel.com/600/600/people/4">
+			<p class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		</a>
+		<a class="carousel-item" href="#five!"
+			><h2 class="center">Machin truc</h2>
+			<img src="http://lorempixel.com/600/600/people/5">
+			<p class="truncate">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit.</p>
+		</a>
+	</div>
+</div>
 <?php $this->stop('main_content') ?>
