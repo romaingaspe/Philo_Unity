1486b4ad3367827638749c660747ea70dee7bee2
martinatVincent <offres@vincentmartinat.com>
2016-02-17 10:12:33 +0100

    h1 index disparait
    obligatoire pour commiter

--- app/templates/front/index.php
+++ app/templates/front/index.php
@@ -1,5 +1,18 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-<h1>INDEX</h1>
+
+<style type="text/css">
+	.image_index img {
+		width: 100%;
+		height: 400px;
+}
+</style>
+
+		<div class="image_index">	
+			<img src="http://static.panoramio.com/photos/original/12271445.jpg">
+		</div>
+
+		<h5>INDEX</h5>
+
 <?php $this->stop('main_content') ?>
