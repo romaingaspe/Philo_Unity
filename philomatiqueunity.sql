70593d3a8194fc9734a6ff5c8a3f3185d11a6e06
GetHobbit <masgeof@gmail.com>
2016-02-18 14:13:24 +0100

    Css base de données et profils

--- philomatiqueunity.sql
+++ philomatiqueunity.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 16 Février 2016 à 16:05
+-- Généré le :  Jeu 18 Février 2016 à 14:10
 -- Version du serveur :  10.1.9-MariaDB
 -- Version de PHP :  5.6.15
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `philomatiqueunity`
 --
+CREATE DATABASE IF NOT EXISTS `philomatiqueunity` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `philomatiqueunity`;
 
 -- --------------------------------------------------------
 
@@ -43,7 +45,8 @@ CREATE TABLE `commentaires` (
 CREATE TABLE `inscrit` (
   `id_user` int(11) NOT NULL,
   `id_metier` int(11) NOT NULL,
-  `date` date NOT NULL
+  `date` date NOT NULL,
+  `specialités` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -53,12 +56,18 @@ CREATE TABLE `inscrit` (
 --
 
 CREATE TABLE `metiers` (
-  `section` enum('Français','Anglais','Arts plastiques','Broderie','Bureautique word/excel/access/powerpoint/libre office 2010','Cartonnage','Coupe modélisme','Couture','Couture Décoration Intérieure','Couture d’Ameublement','Dessin Artistique','Electricité','Encadrement art - initiation','Encadrement art - perfectionnement','Espagnol','Histoire de l''Art du Mobilier','Histoire de la mode et du costume','Installation Sanitaire','Installation Thermique','Menuiserie','Modélisme/coupe','Peinture acrylique','Peinture Artistique','Peinture Bâtiment','Peinture décorative','Peinture huile','Plomberie Sanitaire','Protection santé environnement','Réfection de Sièges','Réfection de sièges / préparation au CAP','Retouches/Customisation','Sculpture','Stylisme') CHARACTER SET latin1 NOT NULL,
+  `section` enum('Arts plastiques','Broderie','Cartonnage','Coupe modélisme','Couture','Couture Décoration Intérieure','Couture d’Ameublement','Dessin Artistique','Electricité','Encadrement art - initiation','Encadrement art - perfectionnement','Espagnol','Histoire de l''Art du Mobilier','Histoire de la mode et du costume','Installation Sanitaire','Installation Thermique','Menuiserie','Modélisme/coupe','Peinture acrylique','Peinture Artistique','Peinture Bâtiment','Peinture décorative','Peinture huile','Plomberie Sanitaire','Protection santé environnement','Réfection de Sièges','Réfection de sièges / préparation au CAP','Retouches/Customisation','Sculpture','Stylisme') NOT NULL,
   `id` int(11) NOT NULL,
   `photo` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `specialite` varchar(255) CHARACTER SET latin1 NOT NULL,
   `description` text CHARACTER SET latin1 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `metiers`
+--
+
+INSERT INTO `metiers` (`section`, `id`, `photo`, `description`) VALUES
+('Broderie', 1, 'https://s-media-cache-ak0.pinimg.com/236x/00/d3/46/00d346b41ec2537b6263598d61f41ca8.jpg', 'La broderie est un art de décoration des tissus qui consiste à ajouter sur un tissu un motif plat ou en relief fait de fils simples, parfois en intégrant des matériaux tels que paillettes, perles voire pierres précieuses.\r\n\r\nOn peut la diviser en trois grandes classes :\r\n\r\nmanuelle : faite à la main, au moyen d''une aiguille ou faite au crochet ;\r\nsemi-mécanique : à la machine ;\r\n mécanique : industrielle.');
 
 -- --------------------------------------------------------
 
