683784a80f06a4c11fc9b76280da764ede445469
GetHobbit <masgeof@gmail.com>
2016-02-18 11:23:26 +0100

    Css layout et footer

--- app/templates/layout.php
+++ app/templates/layout.php
@@ -6,16 +6,15 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/materialize.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
 	<?php $this->insert('partials/header') ?>
-
 	<div>
 		<section>
 			<?= $this->section('main_content') ?>
 		</section>
 	</div>
-
 	<?php $this->insert('partials/footer') ?>
 
 	<script type="text/javascript" src="<?= $this->assetUrl('js/jquery-2.2.0.js') ?>"></script>
--- app/templates/partials/footer.php
+++ app/templates/partials/footer.php
@@ -1,9 +1,14 @@
 
 <footer>
 	<div class="brown">
-		<ul class="center">
-			<li><a href="/Philo_Unity/public/conditions">Lien vers Conditions</a></li>
-			<li><a href="/Philo_Unity/public/contactadmin">Lien vers Contact</a></li>
+		<ul class="center row">
+			<li class="col l6"><i class="fa fa-facebook-square fa-3x"></i></li>
+			<li class="col l6"><i class="fa fa-twitter-square fa-3x"></i></li>
 		</ul>
+		<ul class="center row">
+			<li class="col l6"><a href="/Philo_Unity/public/conditions">Lien vers Conditions</a></li>
+			<li class="col l6"><a href="/Philo_Unity/public/contactadmin">Lien vers Contact</a></li>
+		</ul>
+
 	</div>
 </footer>
