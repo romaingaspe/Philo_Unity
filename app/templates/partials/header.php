5b1966630875b037a022c4a1229e2b5d5a3b3875
GetHobbit <masgeof@gmail.com>
2016-02-18 12:25:24 +0100

    Modif Header et liens

--- app/templates/partials/header.php
+++ app/templates/partials/header.php
@@ -4,15 +4,9 @@
 			<a href="/Philo_Unity/public">
 				<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
 			</a>
-			<ul id="dropdown1" class="dropdown-content">
-			  <li><a href="/Philo_Unity/public/profils">Catégories</a></li>
-			  <li class="divider"></li>
-			  <li><a href="/Philo_Unity/public/metiers">Les métiers</a></li>
-			  <li class="divider"></li>
-			</ul>
 			<ul class="right">
-				<li><a class="dropdown-button" href="#!" data-activates="dropdown1">Métiers<i class="material-icons right">arrow_drop_down</i></a></li>
 				<li><a href="/Philo_Unity/public/profils">Profils</a></li>
+				<li><a href="/Philo_Unity/public/profil/portfolio/allworks">Metiers</a></li>
 				<li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li>
 				<li><a href="/Philo_Unity/public/admin/deconnect">Se deconnecter</a></li>
 			</ul>
