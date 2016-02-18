aef342c0aba50f6010f36773c62a06f025a7a8a6
GetHobbit <masgeof@gmail.com>
2016-02-18 15:44:03 +0100

    Select all de métier

--- app/Controller/MetiersController.php
+++ app/Controller/MetiersController.php
@@ -3,13 +3,18 @@
 namespace Controller;
 
 use \W\Controller\Controller;
+use Manager\MetierManager;
 
 class MetiersController extends Controller
 {
     public function metiers()
 	{
-        
-		$this->show('metiers/metiers');
+
+        $metiersdb = new MetierManager;
+        $metiers = $metiersdb->findAll('section', "ASC", 6);
+        $params['metiers'] = $metiers;
+
+		$this->show('metiers/metiers', $params);
 	}
 
 }
