4ffe0f6cdd3d3048820d3bf18dc4dc2746956060
FatOuch <designdif@live.fr>
2016-02-20 23:43:39 +0100

    Responsive projectsPage+ style.css maj

--- app/templates/portfolio/projectsPage.php
+++ app/templates/portfolio/projectsPage.php
@@ -13,11 +13,11 @@
 	</div>
   <section id="description" class="container section">
     <div class="row">
-      <div id="titleprojet" class="col m6 l4">
+      <div id="titleprojet" class="col s12 m12 l4">
         <h4 style="background-color: #128b82;margin-top: 0 !important;font-size: 1.7em;text-align: center;color: white;">Dessus de table</h4>
         <p style="background-color: #e0f2f1;padding: 10px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus vero dolor quidem possimus dicta consequatur aliquam, soluta minima voluptatibus officia maiores ipsam veritatis dolore dolorum eum, eligendi modi a commodi?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, pariatur, dignissimos. Assumenda rem dolorum corporis unde eveniet hic voluptas repudiandae eaque illo velit cupiditate porro, non blanditiis consectetur totam ullam?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam perferendis eaque perspiciatis harum neque, repudiandae ducimus saepe tenetur consectetur nostrum. Enim necessitatibus quos, dolorem? Impedit explicabo cumque voluptatum tempora Lorem ipsum dolor sit amet, consectetur </p>
       </div>
-      <div id="slideprojet" class="col m6 l8 slider">
+      <div id="slideprojet" class="col s12 m12 l8 slider">
           <ul class="slides">
             <li>
               <img src="http://lorempixel.com/580/250/nature/1"> <!-- random image -->
@@ -48,22 +48,22 @@
               </div>
             </li>
           </ul>
-          <div>
-              <a class="waves-effect waves-light btn right btn-com-project">commenter</a>
+          <div id="comment-btn" class="col s12 m12 l12 ">
+              <a class="waves-effect waves-light btn right link-com-project">commenter</a>
           </div>
       </div>
       
     </div>
     
   </section>
-  <section class="container">
+  <section class="container comments-bloc">
     <h5 class="center-align">Derniers commentaires sur le projet</h5>
-    <div id="com-projects" class="grey lighten-2 ">
+    <div id="com-project" class="grey lighten-2 ">
   			<h7>gorginette</h7>
   			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus accusamus quaerat quam quibusdam modi 		enim, fugit! Quod voluptatem quisquam ducimus dolorem nisi blanditiis vitae iste error. Minima necessitatibus, natus minus.</p>
   			<p style="font-style: italic;font-size: 0.8em;">posté le 02/03/2156</p>			
   	</div>
-  	<div id="com-projects" class="grey lighten-2">
+  	<div id="com-project" class="grey lighten-2">
   			<h7>pascalette</h7>
   			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus accusamus quaerat quam quibusdam modi enim, fugit! Quod voluptatem quisquam ducimus dolorem nisi blanditiis vitae iste error. Minima necessitatibus, natus minus.</p>
   			<p style="font-style: italic;font-size: 0.8em;">posté le 02/04/2156</p>	
