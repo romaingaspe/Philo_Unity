cc3731c59ae65620ad1081c2607fe201b7afc532
FatOuch <designdif@live.fr>
2016-02-20 16:32:15 +0100

    Responseive contactadmin +footer (style.css maj)

--- app/templates/front/contactAdmin.php
+++ app/templates/front/contactAdmin.php
@@ -1,28 +1,31 @@
 <?php $this->layout('layout2', ['title' => 'Contact']) ?>
 
 <?php $this->start('main_content') ?>
-<div class="container section center" style="margin-top:50px">
-    <div class="row">
+<div class="container section center margetop">
+    <div class="col s6 m8 l10">
         <h2>Contacter l'administrateur</h2>
-        <form class="col l8 offset-l2" style="margin-top:50px">
+    </div>
+    <div class="row">
+        
+        <form class="col s6 offset-s3 col m6 offset-m3 col l6 offset-l3 margetop">
             <div class="row">
-                <div class="input-field col l6">
+                <div class="input-field col s12 m12 l6">
                     <input placeholder="Prénom" id="prénom" type="text" class="validate">
                     <label for="prénom">Prénom</label>
                 </div>
-                <div class="input-field col l6">
+                <div class="input-field col s12 m12 l6">
                     <input placeholder="Nom" id="nom" type="text" class="validate">
                     <label for="nom">Nom</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col l12">
+                <div class="input-field col s12 m12 l12">
                     <input placeholder="Objet du message" id="objet" type="text" class="validate">
                     <label for="objet">Objet</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col l12">
+                <div class="input-field col s12 m12 l12">
                     <textarea id="textarea1" class="materialize-textarea"></textarea>
                     <label for="message">Message</label>
                 </div>
