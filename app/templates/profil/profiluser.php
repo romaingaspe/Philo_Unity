c0eeae9c7eedf9c06e6166c01649facb01bee366
FatOuch <designdif@live.fr>
2016-02-21 18:44:28 +0100

    Ajout date d'inscription

--- app/templates/profil/profiluser.php
+++ app/templates/profil/profiluser.php
@@ -13,7 +13,7 @@
             <?php
             echo '<h2>'.$profil['prenom'].' '.$profil['nom'].'</h2>';
             echo '<h3>Métier</h3>';
-            echo '<p>date</p>';
+            echo '<p>inscrite le '. $profil['date_update'].'</p>';
             echo '<br />';
             echo '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
             ?>
