97af0d9ac446891b8b4db469e20c647275c250cc
defelite <lanfranchi.guillaume@yahoo.com>
2016-02-21 11:25:20 +0100

    responsive nav.
    style a valider par tout le monde.

--- app/templates/partials/header.php
+++ app/templates/partials/header.php
@@ -4,7 +4,7 @@
 			<a href="/Philo_Unity/public">
 				<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
 			</a>
-			<ul class="right">
+			<ul id="nav-resp" class="row col s12 m6 l3 right">
 				<li><a href="/Philo_Unity/public/profils">Profils</a></li>
 				<li><a href="/Philo_Unity/public/metiers?page=1">Metiers</a></li>
 				<?php if(!$_SESSION) :?><li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li><?php endif;?>
