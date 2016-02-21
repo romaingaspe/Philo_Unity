4260c6d51a2d1ee171a61998f5782c2fc0baaa8c
GetHobbit <masgeof@gmail.com>
2016-02-21 16:58:48 +0100

    Infos compte Nav

--- app/templates/partials/header.php
+++ app/templates/partials/header.php
@@ -1,31 +1,28 @@
 <header>
-	<?php var_dump($_SESSION);?>
 	<nav class="brown">
 		<div class="container nav-wrapper">
 			<a href="/Philo_Unity/public">
 				<img class="left" src="<?= $this->assetUrl('img/logo_final.png') ?>"></img>
 			</a>
+			<ul id="dropdown1" class="dropdown-content">
+			  <li><a href="<?php echo '/philo_unity/public/profil/profiluser/'.$_SESSION['user']['id'] ?>">Mon profil</a></li>
+			  <li><a href="#!">Mon Portfolio</a></li>
+			  <li class="divider"></li>
+			  <li><a href="<?= $this->url('update') ?>">Mes infos</a></li>
+			</ul>
 			<ul id="nav-resp" class=" col s12 m6 l4 right">
 				<li><a href="/Philo_Unity/public/metiers?page=1">Metiers</a></li>
 				<?php if(!$_SESSION) :?><li><a href="/Philo_Unity/public/admin/connect">Se connecter</a></li><?php endif;?>
 				<?php if($_SESSION) :?><li><a href="/Philo_Unity/public/admin/deconnect">Se deconnecter</a></li><?php endif;?>
+				<?php if($_SESSION) :?>
 					<li>
-						<a href="#">
-							<div class="col l12">
-			        	<div class="lighten-5">
-				          <div class="valign-wrapper">
-										<img src="http://lorempixel.com/50/50" alt="" class="circle valign" />
-											<p>
-												
-											</p>
-						          <span class="valign">
-							          Mon Profil
-						          </span>
-				          </div>
-			        	</div>
-		      		</div>
-						</a>
+						<a href="#" class="col l6 dropdown-button" data-beloworigin="true" data-activates="dropdown1" id="loginfo">
+								<img src="<?php echo $_SESSION['user']['photo'] ?>" alt="" class="left " id="circleprofil"/>
+						&nbsp;&nbsp;&nbsp;Mon profil
+						<i class="material-icons right">arrow_drop_down</i>
+					</a>
 					</li>
+				<?php endif;?>
 			</ul>
 		</div>
 	</nav>
