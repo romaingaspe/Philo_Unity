926260f795352e248d97a82edc2720f2edda8a29
GetHobbit <masgeof@gmail.com>
2016-02-23 14:41:53 +0100

    <A>dans le footer

--- app/templates/partials/footer.php
+++ app/templates/partials/footer.php
@@ -7,8 +7,8 @@
 				<li class="col l4"><i class="fa fa-twitter-square fa-3x"></i></li>
 			</ul>
 			<ul class="right">
-				<li class="col l4"><a href="<?= $this->url('conditions') ?>">Lien vers Conditions</a></li>
-				<li class="col l4"><a href="<?= $this->url('contactAdmin') ?>">Lien vers Contact</a></li>
+				<li class="col l4"><a href="<?= $this->url('conditions') ?>"><u>Lien vers Conditions</u></a></li>
+				<li class="col l4"><a href="<?= $this->url('contactAdmin') ?>"><u>Lien vers Contact</u></a></li>
 			</ul>
 		</div>
 	</div>
