6977d9dd29cf2a52537f3c4ee1cf9f320f6c8787
GetHobbit <masgeof@gmail.com>
2016-02-24 14:30:47 +0100

    Slider Index

--- app/templates/front/index.php
+++ app/templates/front/index.php
@@ -13,19 +13,25 @@
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</p>
 </div>
-<div id="carous" class="">
-	<div class="carousel">
-		<?php foreach ($users as $use):?>
-			<a class="carousel-item" href="/philo_unity/public/profil/profiluser/<?= $use['id'] ?>">
-				<h2 class="center"><?= $use['prenom'].' '.$use['nom']?></h2>
-				<img src="<?= $use['photo'] ?>">
-				<?php if(strlen($use['description']) > 100):?>
-				<p class="center section"><?= mb_substr($use['description'], 0 , 100).'...' ?></p>
-				<?php else:?>
-				<p class="center section"><?=$use['description']?></p>
-				<?php endif;?>
-			</a>
-		<?php endforeach;?>
-	</div>
+<div class="container">
+	<div class="slider louil">
+	    <ul class="slides louil" >
+			<?php foreach ($users as $use): ?>
+				<li class="row">
+				  <img class="col l4"src="<?= $use['photo'];?>">
+			    <div class="teal col l8 slida">
+					<h3><?= $use['nom'].' '.$use['prenom'];?></h3>
+					<?php if (strlen($use['description']) > 50): ?>
+						<h5 class="light grey-text text-lighten-3"><?= mb_substr($use['description'], 0 , 50).'...';?></h5>
+						<?php else: ?>
+							<h5 class="light grey-text text-lighten-3"><?= $use['description'];?></h5>
+					<?php endif; ?>
+				</div>
+				</li>
+			<?php endforeach; ?>
+	    </ul>
+	  </div>
 </div>
+
+
 <?php $this->stop('main_content') ?>
