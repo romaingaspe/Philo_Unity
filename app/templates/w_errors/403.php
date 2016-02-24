ed30c1e37f773a649eeb0b74fac3a753a785ebb4
GetHobbit <masgeof@gmail.com>
2016-02-24 15:47:16 +0100

    403 / 404 faits

--- app/templates/w_errors/403.php
+++ app/templates/w_errors/403.php
@@ -1,5 +1,7 @@
 <?php $this->layout('layout', ['title' => 'Créer un nouvel administrateur']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>403. Nothing to see here.</h1>
+<div class="section container">
+    <h1 class="center">403. Circulez, y'a rien à voir !</h1>
+</div>
 <?php $this->stop('main_content'); ?>
--- app/templates/w_errors/404.php
+++ app/templates/w_errors/404.php
@@ -1,5 +1,8 @@
-<?php $this->layout('layout', ['title' => 'Perdu ?']) ?>
+<?php $this->layout('layout2', ['title' => 'Perdu ?']) ?>
 
 <?php $this->start('main_content'); ?>
-<h1>404. Perdu ?</h1>
+<div class="section container">
+    <h1 class="center">404. Perdu ?</h1>
+</div>
+
 <?php $this->stop('main_content'); ?>
