0fe6fc9710c8f680ca8ffb613e0fec050ad66bcd
defelite <lanfranchi.guillaume@yahoo.com>
2016-02-25 16:56:10 +0100

    commit

--- app/templates/front/index.php
+++ app/templates/front/index.php
@@ -8,7 +8,7 @@
 		<img id="resp-logo" class="col s12" src="<?= $this->assetUrl('img/LogoPhilomathique.png') ?>" alt="" />
 	</div>
 </div>
-<div class="row section center container" id="presentation">
+<div id="titre-index" class="row section center container">
 	<p class="grey lighten-4 z-depth-1 center-align">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</p>
