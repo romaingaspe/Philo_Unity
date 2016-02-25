fc8bbd1a5091a4eb2ff3697e8befa4df0390e728
GetHobbit <masgeof@gmail.com>
2016-02-25 11:39:35 +0100

    Débug commentaire et project page

--- app/templates/portfolio/commentaires.php
+++ app/templates/portfolio/commentaires.php
@@ -43,6 +43,9 @@
       			</div>
       		</form>
     		</form>
-  			</div>			
+  			</div>
 		</div>
 <?php $this->stop('main_content') ?>
+
+<?php $this->start('script') ?>
+<?php $this->stop('script') ?>
--- app/templates/profil/projectsPage.php
+++ app/templates/profil/projectsPage.php
@@ -11,13 +11,13 @@
       </span>
     </div>
   </div>
- 
+
 
   <section id="description" class="container section">
     <div class="row">
       <div id="titleprojet" class="col s12 m12 l4">
-        <h4><?= $projet['project_title']?></h4>
-        <p><?= $projet['description']?></p>
+        <h4><?= $projets['project_title']?></h4>
+        <p><?= $projets['description']?></p>
       </div>
       <div id="slideprojet" class="col s12 m12 l8 slider">
         <ul class="slides">
@@ -26,7 +26,7 @@
               <img src="<?= $pictures['photo']?>"> <!-- random image -->
               <div class="caption center-align">
                 <h3><?= $pictures['caption']?></h3>
-                <h5 class="light grey-text text-darken-2">Projet<?= $projet['project_title']?></h5>
+                <h5 class="light grey-text text-darken-2">Projet<?= $projets['project_title']?></h5>
                 </div>
             </li>
           <?php endforeach;?>
@@ -40,7 +40,7 @@
   <section class="container comments-bloc">
 
     <!-- formulaire -->
-    <?php if($w_user) :?> 
+    <?php if($w_user) :?>
       <div id="form-com"class="">
         <div class="row">
 
@@ -51,22 +51,22 @@
               <div id="com-project" class="grey lighten-2 ">
                   <h7><?= $coms['titre']?></h7>
                   <p><?= $coms['comments']?></p>
-                  <p class="date-publi">publié le<?= $coms['date']?></p>     
+                  <p class="date-publi">publié le<?= $coms['date']?></p>
               </div>
-            <?php endforeach;?> 
+            <?php endforeach;?>
           </div>
           <!-- Pb d'ajout de texte intempestif en base de données
           J'essaie de régler ce problème, je teste donc avec un nouveau contenu de texte si ma page se recharge sans rajouter du texte en base de données... -->
-          
+
           <form class="col s12 m12 l8" method="POST" action="<?= $this->url('projectsPage',['id' => $projet['id']])?>">
             <?php if($formError):?>
               <p class="error"><?= $erreurs ?></p>
-            <?php endif;?>  
+            <?php endif;?>
 
             <?php if($formValid):?>
               <p class="valid">Votre commentaire a bien été envoyé !</p>
-            <?php endif;?>  
-    
+            <?php endif;?>
+
             <h5 class="container center-align ">Publier un commentaire </h5>
             <div class="row">
               <div class="input-field col s12 m12 l12">
@@ -82,20 +82,20 @@
             <div class="col s12 m12 l12">
                 <button type="submit" class="waves-effect waves-light btn right">Envoyer</button>
             </div>
-          </form>  
-          </div>  
+          </form>
+          </div>
         </div>
-    <?php endif;?>  
-
+    <?php endif;?>
+    <?php var_dump($user)?>
     <?php if(!$w_user) :?><!-- zone de commentaires où j'afficherai 'les commentaires', si je ne suis pas inscrit -->
       <?php foreach($commentaires as $coms):?>
-          <h5 class="center-align">Commentaire de <?= $utilisateur['prenom'].' '.$utilisateur['nom']?></h5>
+          <h5 class="center-align">Commentaire de <?= $user['prenom'].' '.$user['nom']?></h5>
           <div id="com-project" class="grey lighten-2 ">
               <h7><?= $coms['titre']?></h7>
               <p><?= $coms['comments']?></p>
-              <p class="date-publi">publié le<?= $coms['date']?></p>     
+              <p class="date-publi">publié le<?= $coms['date']?></p>
           </div>
-        <?php endforeach;?> 
+        <?php endforeach;?>
     <?php endif;?>
   </section>
 
