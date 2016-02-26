af74d099db1f8d2d49b627d13138abe4197014bc
GetHobbit <masgeof@gmail.com>
2016-02-26 10:39:43 +0100

    Style recherche

--- app/templates/front/recherche.php
+++ app/templates/front/recherche.php
@@ -10,26 +10,36 @@
 <section id="allworks" class="row section container">
 <h2 class="center">Résultats :</h2>
 <?php foreach ($resultatUser as $resuse):?>
-	<article class="col s12 m6 l4">
-		<div>
-			<img class="photo-work" src="<?= $resuse['photo']?>" alt="">
-		</div>
-		<div class="text-works">
-			<h6><?= $resuse['prenom'].' '.$resuse['nom']?></h6>
-			<p><?= $resuse['description']?></p>
-			<a href="/philo_unity/public/profil/profiluser/<?= $resuse['id']?>">Visiter le Profil</a>
+	<article class="grey lighten-4 col s12 m6 l4">
+		<div class="grey lighten-4">
+			<div class=" contain-img">
+				<img class="hov-zoom photo-work responsive-img" src="<?=  $this->assetUrl($resuse['photo'])?>" alt="">
+				<div class="text-box ">
+					<h2>Visiter le Profil</h2>
+					<a class="link-metier" href="/philo_unity/public/profil/profiluser/<?= $resuse['id']?>"></a>
+				</div>
+			</div>
+			<div class="text-works center">
+				<h6><?= $resuse['prenom'].' '.$resuse['nom']?></h6>
+				<p class="grey lighten-4"><?= mb_substr($resuse['description'], 0 , 400 )?></p>
+			</div>
 		</div>
 	</article>
 <?php endforeach;?>
 <?php foreach ($resultatMetier as $resmet):?>
 	<article class="col s12 m6 l4">
-		<div>
-			<img class="photo-work" src="<?= $resmet['photo']?>" alt="">
-		</div>
-		<div class="text-works">
-			<h6><?= $resmet['section']?></h6>
-			<p><?= $resmet['description']?></p>
-			<a href="/philo_unity/public/metier/<?= $resmet['alias']?>/profilsall">Visiter le Profil</a>
+		<div class="grey lighten-4 z-depth-1">
+			<div class=" contain-img">
+				<img class="hov-zoom" src="<?= $resmet['photo']?>" alt="">
+				<div class="text-box">
+					<h2 class="lighten-4">Voir les profils</h2>
+					<a class="link-metier" href="metiers/<?= $resmet['alias']?>/profilsall"></a>
+				</div>
+			</div>
+			<div class="text-works center">
+				<h6><?= $resmet['section']?></h6>
+				<p class=""><?= mb_substr($resmet['description'], 0 , 400 ).'...'?></p>
+			</div>
 		</div>
 	</article>
 <?php endforeach;?>
